0d0087ca25c4eb99851c9c9fab1e122f22d23386
András Németh <sunarch@protonmail.com>
2023-07-02 16:03:26 +0200

    Add file headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
